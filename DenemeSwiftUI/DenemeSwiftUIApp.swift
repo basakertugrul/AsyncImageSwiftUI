35ed48564d088560d51c50fae5c9f2d40c5e9023
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2021-11-14 00:34:57 +0300

    “Initial”

---
+++ DenemeSwiftUI/DenemeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenemeSwiftUIApp.swift
+//  DenemeSwiftUI
+//
+//  Created by Başak Ertuğrul on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DenemeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
